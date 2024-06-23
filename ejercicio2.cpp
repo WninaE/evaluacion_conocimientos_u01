ab0de2f7b0590a3648ebff4a009ad65e7258cb91
WninaE <wninae@unjbg.edu.pe>
2024-06-23 17:20:10 -0500

    Actualizacion del segundo ejericio

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -4,20 +4,25 @@
 using namespace std;
 int main(){
     int n1, n2, c, i;
-    cin>>n1; cin>>n2;
+    cout<<"Ingrese el primer numero:"; cin>>n1; 
+    cout<<"Ingrese el segundo numero:";cin>>n2;
     c=0;
     i=1;
-    while(i<=n1 && i<=n2){
-        if(n1%i==0 && n2%i==0){
-            c=c+1;
+    if(n1>=1 && n2>=1){
+        while(i<=n1 && i<=n2){
+            if(n1%i==0 && n2%i==0){
+                c=c+1;
+            }
+            i=i+1;
         }
-        i=i+1;
-    }
-    if(c>1){
-        cout<<"No son PESI"<<endl;
+        if(c>1){
+            cout<<"No son PESI"<<endl;
+        }
+        else{
+            cout<<"Son PESI"<<endl;
+        }
     }
     else{
-        cout<<"Son PESI"<<endl;
+        cout<<"Numero(s) no admitido(s), ingrese 2 numeros positivos";
     }
     return 0;
-}
